5f6a3115479aa97832cb0d368fd960edda657c92
Marc O'Morain <github.com@marcomorain.com>
2013-05-19 20:54:55 +0100

    tokens

--- crush/main.c
+++ crush/main.c
@@ -4,20 +4,91 @@
 
 
 struct tokeniser;
-typedef void(*state)(struct tokeniser*);
+typedef int(*state)(struct tokeniser*);
 
 struct tokeniser {
     FILE* input;
     unsigned current;
+    unsigned next;
     state state;
 };
 
-
-void state_data(struct tokeniser* t)
+enum
 {
-    switch(t->current)
+    TOKEN_IDENT,
+    TOKEN_FUNCTION,
+    TOKEN_AT_KEYWORD,
+    TOKEN_HASH,
+    TOKEN_STRING,
+    TOKEN_BAD_STRING,
+    TOKEN_URL,
+    TOKEN_BAD_URL,
+    TOKEN_DELIM,
+    TOKEN_NUMBER,
+    TOKEN_PERCENTAGE,
+    TOKEN_DIMENSION,
+    TOKEN_UNICODE_RANGE,
+    TOKEN_INCLUDE_MATCH,
+    TOKEN_DASH_MATCH,
+    TOKEN_PREFIX_MATCH,
+    TOKEN_SUFFIX_MATCH,
+    TOKEN_SUBSTRING_MATCH,
+    TOKEN_COLUMN,
+    TOKEN_WHITESPACE,
+    TOKEN_CDO,
+    TOKEN_CDC,
+    TOKEN_COLON,
+    TOKEN_SEMICOLON,
+    TOKEN_COMMA,
+    TOKEN_LEFT_SQUARE,
+    TOKEN_RIGHT_SQUARE,
+    TOKEN_PAREN_LEFT,
+    TOKEN_PAREN_RIGHT,
+    TOKEN_LEFT_CURLY,
+    TOKEN_RIGHT_CURLY
+};
+
+int state_double_quoted_string(struct tokeniser* t)
+{
+    return 0;
+}
+
+int state_single_quoted_string(struct tokeniser* t)
+{
+    return 0;
+}
+
+int state_data(struct tokeniser* t)
+{
+    switch(t->next)
     {
+        case '"':
+            t->state = state_double_quoted_string;
+            break;
+
+        case '\'':
+            t->state = state_single_quoted_string;
+            break;
+
+        case '(':
+            // skip
+            return TOKEN_PAREN_LEFT;
+
+        case ')':
+            // skip
+            return TOKEN_PAREN_RIGHT;
+
+        case ',':
+            // skip
+            return TOKEN_COMMA;
+
+        case ':':
+            // skip
+            return TOKEN_COLON;
+            
     }
+
+    return 0;
 }
 
 void tokeniser_init(struct tokeniser* t, FILE* input)
